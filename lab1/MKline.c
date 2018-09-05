08f15bc966761c0ff604c15da40f249c497eff79
Matthew Kline <mkline@mail.umw.edu>
2018-09-05 17:41:00 -0400

    updated work

--- lab1/MKline.c
+++ lab1/MKline.c
@@ -10,7 +10,7 @@ struct LL{
 	struct LL *next;
 };
 typedef struct LL *node; //making a pointer
-
+node *head = NULL;
 node createNode(){
 	node temp;
 	temp = node(malloc(sizeof(struct LL));
@@ -18,22 +18,25 @@ node createNode(){
 	return temp;
 }
 
-node addNode(node head, int x){
+void addNode(node head, int x){
 	node temp;
 	node p;
 	temp = createNode();
-	temp->data = value;
+	temp->data = x;
 	if(head == NULL){
 		head = temp;
 	}
 	else{
-		p = head;
+		/*p = head;
 		while(p->next != NULL){
 			p = p->next; //makes current Node last node
 		}
-		p->next = temp;
+		p->next = temp; */
+		p = head;
+		head = temp;
+		head->next = p;
 	}
-	return head;
+	//return head;
 }
 
 int isEmpty(node head){
@@ -43,7 +46,7 @@ int isEmpty(node head){
 		return 0;
 }
 
-node popNode(node head){
+void popNode(node head){
 	node temp;
 	if(head != NULL){
 		temp = head;
@@ -51,7 +54,42 @@ node popNode(node head){
 	}
 	else
 		head = NULL;
-	return head;
+	//return head;
 }
 
+int main(argc, char *argv[]){
+	int x = 0;
+	int y;
+	printf("This program allows you to add, remove, and check to see if a linked list is empty.\n");
+	while(x != 4){
+		printf(" To add a number to the list press 1 \n To remove the top node from the list press 2 \n To check if the list is empty press 3 \n To exit this program press 4./n");
+ 		scanf("%d",&x);
+		if(x == 1){
+			printf("What number would you like to add to the list?\n");
+			scanf("%d",&y);
+			//add shit
+		}
+		else if(x == 2){
+			//remove shit
+		}
+		else if(x == 3){
+			int b;
+			b = isEmpty();
+			if(b == 1){
+				printf("The list is empty\n");
+			}
+			else{
+				printf("The list has at least 1 item");
+			}
+		}
+		else if(x == 4){
+			printf("Goodbye/n");
+		}
+		else{
+			printf("You failed to input a correct option.\n");
+		}
 
+
+	}
+	return 0; 
+}
