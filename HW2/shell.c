8827f52763801549a23e70f6ae721138fc9e2e98
Matt Kline <mdekline@gmail.com>
2018-10-05 14:53:16 +0000

    more updates

--- HW2/shell.c
+++ HW2/shell.c
@@ -4,13 +4,18 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
+char word1(char *inputline);
+char word2(char *inputline);
+int count;
 int main(){
 	char inputline[256]; //user input
+	char one[256];
+	char two[256];
 	char *args[32];
 	int i=0;
 	int pid;
 	char error_message[30] = "An error has occurred\n";
+	char cwd[256];
 	
 	while(strcmp(inputline, "exit") != 0){
 		printf("mysh> ");
@@ -37,14 +42,20 @@ int main(){
 			//	printf("\n In the child process about to run %s\n", *args);
 			
 			//	execvp(*args, args);
-				if(strcmp(inputline,"exit")==0)
+				if(strcmp(inputline,"exit")==0){
 					printf("Goodbye\n");
+					exit(0);
+				}
+
 				else if(strcmp(inputline,"cd") == 0){
 					chdir(getenv("HOME"));
+					getcwd(cwd,sizeof(cwd));
+					printf("Current working directory is: %s\n", cwd);
 				}
-				//else if(strcmp(inputline, "pwd")==0){
-				
-				//}
+				else if(strcmp(inputline, "pwd")==0){
+					getcwd(cwd, sizeof(cwd));
+					printf("Current working directory is: %s\n",cwd);
+				}
 				else if(execvp(*args, args) < 0){
 					write(STDERR_FILENO, error_message, strlen(error_message));
 				}
@@ -55,11 +66,28 @@ int main(){
 			}
 			else{
 				wait(NULL);
+				if(strcmp(inputline,"cd")==0){
+					chdir(getenv("HOME"));
+					getcwd(cwd,sizeof(cwd));
+				}
 			}
 		}
 		//printf("Input before while: %s\n", inputline);
-
-		i++;
+		
+	
 	}
 	return(0);
+}
+
+char word1(char *inputline){
+	count =0;
+	int i = 0;
+	int start;
+	int end;
+	for(count; count< strlen(buffer); count++){
+		while(i ==0){
+			ch = inputline[count];
+			if//get white spaces
+	}
+
 }
