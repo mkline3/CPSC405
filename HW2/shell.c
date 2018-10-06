7989fd4f17f01718bf5ead9e13b8d9b18d8561c3
Matthew Kline <mkline@mail.umw.edu>
2018-10-05 20:05:16 -0400

    final updates for shell

--- HW2/shell.c
+++ HW2/shell.c
@@ -4,90 +4,215 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-char word1(char *inputline);
-char word2(char *inputline);
-int count;
-int main(){
+
+int main(int argc,char* argv[] ){
 	char inputline[256]; //user input
-	char one[256];
-	char two[256];
 	char *args[32];
 	int i=0;
 	int pid;
 	char error_message[30] = "An error has occurred\n";
 	char cwd[256];
-	
-	while(strcmp(inputline, "exit") != 0){
-		printf("mysh> ");
-		fgets(inputline, sizeof(inputline), stdin);
-		
-		inputline[sizeof(inputline)-1] ='\0';
-		
-		int j=0;
-		while(inputline[j] !='\n'){
-			j++;
-		}
-		inputline[j] = '\0';
-		*args = inputline;
-
-		args[1] = NULL;
-
-		pid = fork();
-		if(pid < 0){
-			printf("\nThe fork Failed\n");
-			return 0;
+	if(argc >1){
+		printf("\tBatch Mode:\n");
+		FILE* file = fopen(argv[1], "r");
+		if(file == NULL){
+			printf("Error with file\n");
 		}
 		else{
-			if(pid == 0){
-			//	printf("\n In the child process about to run %s\n", *args);
-			
-			//	execvp(*args, args);
-				if(strcmp(inputline,"exit")==0){
-					printf("Goodbye\n");
-					exit(0);
-				}
+			while(strcmp(inputline, "exit") != 0){
+				printf("mysh> ");
+				fgets(inputline, sizeof(inputline), file);
+				printf("%s", inputline);
+				inputline[sizeof(inputline)-1] ='\0';
 
-				else if(strcmp(inputline,"cd") == 0){
-					chdir(getenv("HOME"));
-					getcwd(cwd,sizeof(cwd));
-					printf("Current working directory is: %s\n", cwd);
+				int j=0;
+				while(inputline[j] !='\n'){
+					j++;
 				}
-				else if(strcmp(inputline, "pwd")==0){
-					getcwd(cwd, sizeof(cwd));
-					printf("Current working directory is: %s\n",cwd);
-				}
-				else if(execvp(*args, args) < 0){
-					write(STDERR_FILENO, error_message, strlen(error_message));
+				inputline[j] = '\0';
+				*args = inputline;
+
+				args[1] = NULL;
+
+				pid = fork();
+				if(pid < 0){
+					printf("\nThe fork Failed\n");
+					return 0;
 				}
 				else{
-					execvp(*args, args);
+					if(pid == 0){
+						//	printf("\n In the child process about to run %s\n", *args);
+
+						//	execvp(*args, args);
+						if(strcmp(inputline,"exit")==0){
+							printf("Goodbye\n");
+							exit(0);
+						}
+
+						else if(strcmp(inputline,"cd") == 0){
+							chdir(getenv("HOME"));
+							getcwd(cwd,sizeof(cwd));
+							printf("%s\n", cwd);
+						}
+						else if(strcmp(inputline, "pwd")==0){
+							getcwd(cwd, sizeof(cwd));
+							printf( "%s\n",cwd);
+						}
+						else if(strstr(inputline,"cd") != NULL){
+							char *t;
+							int i;
+							t = strtok(inputline, " ");
+							for (i=0; t != NULL; i++) {
+								if(i == 1){
+									if(chdir(t) == 0){
+										getcwd(cwd,sizeof(cwd));
+										printf("%s\n",cwd);
+									}
+									else{
+										write(STDERR_FILENO, error_message, strlen(error_message));
+									}
+								}
+								t = strtok(NULL, " ");
+							}
+
+
+						}
+						else if(execvp(*args, args) < 0){
+							write(STDERR_FILENO, error_message, strlen(error_message));
+						}
+						else{
+							execvp(*args, args);
+						}
+						return(0);
+					}
+					else{
+						wait(NULL);
+						if(strcmp(inputline,"cd")==0){
+							chdir(getenv("HOME"));
+							getcwd(cwd,sizeof(cwd));
+						}
+						else if(strstr(inputline,"cd") != NULL){
+							char *t;
+							int i;
+							t = strtok(inputline, " ");
+							for (i=0; t != NULL; i++) {
+								if(i == 1){
+									if(chdir(t) == 0){
+										getcwd(cwd,sizeof(cwd));
+									}
+									else{
+									//	write(STDERR_FILENO, error_message, strlen(error_message));	
+									}
+								}
+								t = strtok(NULL, " ");
+							}
+
+
+						}
+					}
 				}
-				return(0);
+			}		//printf("Input before while: %s\n", inputline);
+		}
+		fclose(file);
+	}					
+	else{
+		while(strcmp(inputline, "exit") != 0){
+			printf("mysh> ");
+			fgets(inputline, sizeof(inputline), stdin);
+
+			inputline[sizeof(inputline)-1] ='\0';
+
+			int j=0;
+			while(inputline[j] !='\n'){
+				j++;
+			}
+			inputline[j] = '\0';
+			*args = inputline;
+
+			args[1] = NULL;
+
+			pid = fork();
+			if(pid < 0){
+				printf("\nThe fork Failed\n");
+				return 0;
 			}
 			else{
-				wait(NULL);
-				if(strcmp(inputline,"cd")==0){
-					chdir(getenv("HOME"));
-					getcwd(cwd,sizeof(cwd));
+				if(pid == 0){
+					//	printf("\n In the child process about to run %s\n", *args);
+
+					//	execvp(*args, args);
+					if(strcmp(inputline,"exit")==0){
+						printf("Goodbye\n");
+						exit(0);
+					}
+
+					else if(strcmp(inputline,"cd") == 0){
+						chdir(getenv("HOME"));
+						getcwd(cwd,sizeof(cwd));
+						printf("%s\n", cwd);
+					}
+					else if(strcmp(inputline, "pwd")==0){
+						getcwd(cwd, sizeof(cwd));
+						printf( "%s\n",cwd);
+					}
+					else if(strstr(inputline,"cd") != NULL){
+						char *t;
+						int i;
+						t = strtok(inputline, " ");
+						for (i=0; t != NULL; i++) {
+							if(i == 1){
+								if(chdir(t) == 0){
+									getcwd(cwd,sizeof(cwd));
+									printf("%s\n",cwd);
+								}
+								else{
+									write(STDERR_FILENO, error_message, strlen(error_message));
+								}
+							}
+							t = strtok(NULL, " ");
+						}
+
+
+					}
+					else if(execvp(*args, args) < 0){
+						write(STDERR_FILENO, error_message, strlen(error_message));
+					}
+					else{
+						execvp(*args, args);
+					}
+					return(0);
+				}
+				else{
+					wait(NULL);
+					if(strcmp(inputline,"cd")==0){
+						chdir(getenv("HOME"));
+						getcwd(cwd,sizeof(cwd));
+					}
+					else if(strstr(inputline,"cd") != NULL){
+						char *t;
+						int i;
+						t = strtok(inputline, " ");
+						for (i=0; t != NULL; i++) {
+							if(i == 1){
+								if(chdir(t) == 0){
+									getcwd(cwd,sizeof(cwd));
+								}
+								else{
+								//	write(STDERR_FILENO, error_message, strlen(error_message));	
+								}
+							}
+							t = strtok(NULL, " ");
+						}
+
+
+					}
 				}
 			}
-		}
-		//printf("Input before while: %s\n", inputline);
-		
-	
+			//printf("Input before while: %s\n", inputline);
+		}	
+
 	}
 	return(0);
 }
 
-char word1(char *inputline){
-	count =0;
-	int i = 0;
-	int start;
-	int end;
-	for(count; count< strlen(buffer); count++){
-		while(i ==0){
-			ch = inputline[count];
-			if//get white spaces
-	}
 
-}
